9a2538f6adb79c9b99a214f4a48600d4a19bc55d
Angel1841 <angelatanasov10@gmail.com>
2022-05-02 00:24:21 +0300

    uploaded ShortestWord.java

---
+++ ShortestWord.java
@@ -0,0 +1,19 @@
+public class ShortestWord {
+    public static int findShort(String s) {
+
+        String[] words = s.split(" ");
+        int min = Integer.MAX_VALUE;
+
+        for (String word: words) {
+            if(word.length() < min){
+                min = word.length();
+            }
+        }
+
+        return min;
+    }
+}
+
+//Simple, given a string of words, return the length of the shortest word(s).
+//
+//String will never be empty and you do not need to account for different data types.
